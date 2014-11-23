ba957145efd8340d6a3c9637883c5ca6c6619201
Jiexi Cao <jiexicao@gmail.com>
2014-11-22 16:44:07 -0800

    Added function to push notification to user

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,28 @@
+/**
+ * Given a 'userId' and a 'message' in the request.params, push a notification
+ * to the specified user with the given message.
+ */
+Parse.Cloud.define("pushMsgToUser", function(request, response) {
+    // Get the user object to push to.
+    var userId = request.params.userId;
+    var user = new Parse.User();
+    user.id = userId;
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+    var instQuery = new Parse.Query(Parse.Installation);
+    instQuery.equalTo("user", user);
+
+    Parse.Push.send({
+        where: instQuery,
+        data: {
+            alert: request.params.message
+        }
+    }, {
+        success: function() {
+            response.success("success");
+        },
+        error: function (error) {
+            response.error(error);
+        }
+    });
 });
+
